d07d40b0d58f3bf43b5525c5f7c41763fc065fde
ItaloCobains <italobrandao.contato@gmail.com>
2023-05-31 23:02:38 -0300

    adicionando delegua e testando

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
+import { AvaliadorSintatico, InterpretadorBase, Lexador } from '@designliquido/delegua';
+import CodeEditor, {
+  CodeEditorSyntaxStyles,
+} from '@rivascva/react-native-code-editor';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 50,
+    flex: 1,
+  },
+});
 
 export default function App() {
+  const lexador = new Lexador();
+  const avaliadorSintatico = new AvaliadorSintatico();
+  // eslint-disable-next-line no-console
+  const interpretador = new InterpretadorBase('', false, console.log, console.log);
+
+  const simbolos = lexador.mapear(["escreva('Olá mundo')"], -1);
+  const declaracoes = avaliadorSintatico.analisar(simbolos);
+  const resultado = interpretador.interpretar(declaracoes);
+
+  console.log(resultado);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <CodeEditor
+        style={{
+          fontSize: 20,
+          inputLineHeight: 26,
+          highlighterLineHeight: 26,
+        }}
+        language="javascript"
+        syntaxStyle={CodeEditorSyntaxStyles.dracula}
+        showLineNumbers
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
